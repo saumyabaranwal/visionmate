a4e907da2e7b60d283d78c91a9cd78889a74b703
Ammar Rizvi <rizviammar579@gmail.com>
2026-07-11 18:04:25 +0530

    added camera support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const [image, setImage] = useState("")
 
-  const [get, setGet] = useState("");
-  const [post, setPost] = useState("");
+  async function startCamera() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+      videoRef.current.srcObject = stream;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then((res) => res.json())
-      .then((data) => setGet(data.message));
-  }, []);
-
-  useEffect(() => {
-
-    fetch("http://localhost:8000/", {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json' 
-      },
-      body: JSON.stringify({name:'Ammar'}) 
-    })
-      .then((res) => res.json())
-      .then((data) => setPost(data.message));
-
-
+    startCamera()
   }, [])
+
+  async function clickPhoto() {
+    const canvas = document.createElement("canvas");
+
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      videoRef.current,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+
+    const imageBlob = await new Promise((resolve) =>
+      canvas.toBlob(resolve, "image/jpeg")
+    );
+
+    const imageUrl = URL.createObjectURL(imageBlob);
+    setImage(imageUrl)
+  }
+
 
 
   return (
     <>
-      <h1>{get}</h1>
-      <h1>{post}</h1>
+
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        width={500}
+      />
+
+      <img src={image} alt="" />
+
+      <button onClick={() => { clickPhoto() }}>scan</button>
+
     </>
-  )
+
+
+  );
 }
 
-export default App
+export default App;
